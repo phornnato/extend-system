5948aaa4b6a05b7f9c84723b5c520f6c781fbc88
Phorn Horm <natophorn@gmail.com>
2026-06-06 10:13:09 +0700

    Fix URI stripping on Vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,7 @@
 <?php
- require __DIR__ . '/../public/index.php';
+
+// Fix for Laravel on Vercel to prevent it from stripping the /api prefix
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['PHP_SELF'] = '/index.php';
+
+require __DIR__ . '/../public/index.php';
